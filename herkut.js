8c01181ad7a36b4e203f1ebb9ec007af192a04fe
Simo Sirén <simo.siren1@careeria.fi>
2023-11-12 09:28:14 +0200

    async and pin text check

--- herkut.js
+++ herkut.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
@@ -10,6 +11,20 @@ app.get('/api/herkut', (req, res) => {
     res.json(namit)
     })
 
+
+ // Pinkoodin lukeminen txt tiedostosta pelvelimelta ja lähettäminen selaimelle   
+app.get('/api/getpin', async (req, res) => {
+try {
+    // Read the content of the text file
+    const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+    
+    // Send the file content as the response
+    res.send(savedPin)
+} catch (error) {
+    console.error('Error reading file:', error)
+    res.status(500).send('Internal Server Error')
+}
+})
 
 // Tehdään polkumääritys public kansioon 
 const polku = path.join(__dirname, './public')
